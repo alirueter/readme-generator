071718ffa4899d587f0d8ca7763199dbf4ea3e52
alirueter <rueter.alison@gmail.com>
2020-07-25 17:05:17 -0500

    capture user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,26 @@
-// array of questions for user
-const questions = [
+//capture user input
+const projectDataArgs = process.argv.slice(2, process.argv.length);
+    console.log(projectDataArgs);
 
-];
+const printProjectData = projectDataArr => {
+    projectDataArr.forEach(projectItem => console.log(projectItem));
+};
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+printProjectData(projectDataArgs);
 
-// function to initialize program
-function init() {
+// // array of questions for user
+// const questions = [
 
-}
+// ];
 
-// function call to initialize program
-init();
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
